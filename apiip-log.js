96c70bb7a10475af2a98e31297662cff4fc47899
Abel Ajish <157879711+Abel-Ajish@users.noreply.github.com>
2024-11-12 19:22:41 +0300

    Update apiip-log.js

--- apiip-log.js
+++ apiip-log.js
@@ -1,5 +1,7 @@
 export default function handler(req, res) {
-    const userIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // Check for x-forwarded-for header or use remoteAddress
+    const userIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress;
+
     console.log("Logged IP:", userIP);  // Logs IP to Vercel logs
     res.status(204).end();  // Empty response, no content for user
 }
